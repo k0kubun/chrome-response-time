821cd1bd5f64d8e3ae44dd8676af6e76ae0ae140
Takashi Kokubun <takashikkbn@gmail.com>
2015-01-12 22:33:00 +0900

    Change response time precision

--- profiler.js
+++ profiler.js
@@ -2,7 +2,7 @@
   setTimeout(function() {
     var t = performance.timing;
     var start = t.redirectStart == 0 ? t.fetchStart : t.redirectStart;
-    var time = String(((t.responseEnd - start) / 1000).toPrecision(3)).substring(0, 4);
+    var time = String(((t.responseEnd - start) / 1000).toPrecision(3)).substring(0, 5);
     var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
     chrome[roe].sendMessage({ time: time });
   }, 0);
